1480bd88d51e1bebe31cf8c910842f5faa0f7dfa
NicooiN <niko91@tpu.ru>
2024-05-24 19:29:14 +0300

    Initial commit and add DC.yml

---
+++ src/main/java/ru/sber/test/app/sbertestapp/SberTestAppApplication.java
@@ -0,0 +1,13 @@
+package ru.sber.test.app.sbertestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SberTestAppApplication.class, args);
+	}
+
+}
